d8942668259c559ccc2304fd100fd28ffcdbbee6
AnishTeku <125890790+AnishTeku@users.noreply.github.com>
2023-05-11 22:30:57 +0100

    cluster number and centers created

--- anish_code.py
+++ anish_code.py
@@ -143,3 +143,62 @@ def cluster_number(df, df_normalised):
     print('best n clusters', best_ncluster[0])
 
     return best_ncluster[0]
+
+#clusters and centers to plot the centers of given data
+
+
+def clusters_and_centers(df, ncluster, y1, y2):
+    '''
+    This function will plot clusters and its centers for given data
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Data for which clusters and centers will be plotted.
+    ncluster : INT
+        Number of clusters.
+    y1 : INT
+        Column 1
+    y2 : INT
+        Column 2
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        Data with cluster labels column added.
+    cen : array
+        Cluster Centers.
+
+    '''
+    # set up the clusterer with the number of expected clusters
+    kmeans = cluster.KMeans(n_clusters=ncluster)
+
+    # Fit the data, results are stored in the kmeans object
+    kmeans.fit(df)
+
+    labels = kmeans.labels_
+    df['labels'] = labels
+    # extract the estimated cluster centres
+    cen = kmeans.cluster_centers_
+
+    cen = np.array(cen)
+    xcen = cen[:, 0]
+    ycen = cen[:, 1]
+
+    # cluster by cluster
+    plt.figure(figsize=(8.0, 8.0))
+
+    cm = plt.cm.get_cmap('tab10')
+    sc = plt.scatter(df[y1], df[y2], 10, labels, marker="o", cmap=cm)
+    plt.title('CLUSTERS OF COUNTIRES OVER LIFE EXPECTANCY IN 1960 AND 2020')
+    plt.scatter(xcen, ycen, 45, "k", marker="s")
+    plt.legend(*sc.legend_elements(), title='clusters')
+    plt.xlabel(f"Life Expectancy({y1})")
+    plt.ylabel(f"Life Expectancy({y2})")
+
+    plt.show()
+
+    print()
+    print(cen)
+
+    return df, cen
