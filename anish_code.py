c193bae78a0418a8c5a6e1139828675ff6a386da
AnishTeku <125890790+AnishTeku@users.noreply.github.com>
2023-05-11 22:45:02 +0100

    added logistic and forecast life expectancy

--- anish_code.py
+++ anish_code.py
@@ -202,3 +202,129 @@ def clusters_and_centers(df, ncluster, y1, y2):
     print(cen)
 
     return df, cen
+
+#logistic function calculation with scale factor
+
+
+def logistic(t, n0, g, t0):
+    """Calculates the logistic function with scale factor n0 
+    and growth rate g"""
+
+    f = n0 / (1 + np.exp(-g*(t - t0)))
+
+    return f
+
+#forecast life expectancy to optimize data of selected country
+
+
+def forecast_life_expectancy(df, country, start_year, end_year):
+    '''
+    This function will analyse data and optimize to forecast 
+    life_expectancy of selected 
+    country
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Data for which forecasting analysis is performed.
+    country : STR
+        Country for which forecasting is performed.
+    start_year : INT
+        Starting year for forecasting.
+    end_year : INT
+        Ending year for forecasting.
+
+    Returns
+    -------
+    None.
+
+    '''
+    df = df.loc[:, country]
+    df = df.dropna(axis=0)
+
+    df_life_expectancy = pd.DataFrame()
+
+    df_life_expectancy['Year'] = pd.DataFrame(df.index)
+    df_life_expectancy['Life_expectancy'] = pd.DataFrame(df.values)
+    df_life_expectancy["Year"] = pd.to_numeric(df_life_expectancy["Year"])
+    importlib.reload(opt)
+
+    param, covar = opt.curve_fit(logistic, df_life_expectancy["Year"],
+                                 df_life_expectancy["Life_expectancy"],
+                                 p0=(1.2e12, 0.03, 1990.0))
+
+    sigma = np.sqrt(np.diag(covar))
+
+    year = np.arange(start_year, end_year)
+    forecast = logistic(year, *param)
+    low, up = err.err_ranges(year, logistic, param, sigma)
+    plt.figure()
+    plt.plot(df_life_expectancy["Year"], df_life_expectancy["Life_expectancy"],
+             label="Life_expectancy", color='b')
+    plt.plot(year, forecast, label="forecast", color='k')
+    plt.fill_between(year, low, forecast, color="red", alpha=0.7)
+    plt.fill_between(year, forecast, up, color="green", alpha=0.7)
+    plt.title(f'LIFE EXPECTANCY PER CAPITA FORECAST FOR {country.upper()}')
+    plt.legend(loc='upper left')
+    plt.xlabel("YEAR")
+    plt.ylabel("LIFE EXPECTANCY in YEARS")
+    plt.savefig(f'{country}.png', bbox_inches='tight', dpi=300)
+    plt.show()
+
+    life_expectancy2030 = logistic(2030, *param)/1e9
+
+    low, up = err.err_ranges(2030, logistic, param, sigma)
+    sig = np.abs(up-low)/(2.0 * 1e9)
+    print()
+    print(f"Life_expectancy 2030 of {country}", life_expectancy2030*1e9,
+          "+/-", sig*1e9)
+
+
+#Reading life_expectancy Data
+df_life_expectancy = read_file("life_expectancy_years.csv")
+print(df_life_expectancy.describe())
+
+#Finding transpose of life_expectancy Data
+df_life_expectancy_tr = transpose_file(df_life_expectancy)
+print(df_life_expectancy_tr.head())
+
+#Selecting years for which correlation is done for further analysis
+df_le = df_life_expectancy[['1960', "1970", "1980", "1990", "2000", "2010",
+                            '2020']]
+print(df_le.describe())
+
+correlation_and_scattermatrix(df_le)
+year1 = "1960"
+year2 = "2020"
+
+# Extracting columns for clustering
+df_ex = df_le[[year1, year2]]
+df_ex = df_ex.dropna()
+
+# Normalising data and storing minimum and maximum
+df_norm, df_min, df_max = ct.scaler(df_ex)
+
+print()
+print("Number of Clusters and Scores")
+ncluster = cluster_number(df_ex, df_norm)
+
+df_norm, cen = clusters_and_centers(df_norm, ncluster, year1, year2)
+
+#Applying backscaling to get actual cluster centers
+scen = ct.backscale(cen, df_min, df_max)
+print('scen\n', scen)
+
+df_ex, scen = clusters_and_centers(df_ex, ncluster, year1, year2)
+
+print()
+print('Countries in last cluster')
+print(df_ex[df_ex['labels'] == ncluster-1].index.values)
+
+#Forecast life_expectancy for India
+forecast_life_expectancy(df_life_expectancy_tr, 'India', 1960, 2031)
+
+#Forecast life_expectancy for United States
+forecast_life_expectancy(df_life_expectancy_tr, 'United States', 1960, 2031)
+
+#Forecast life_expectancy for Pakistan
+forecast_life_expectancy(df_life_expectancy_tr, 'Pakistan', 1960, 2031)
